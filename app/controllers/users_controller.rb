0231b763acc398fbf56ec308bb0ab7961c99bb4f
Cody Chang <codyzchang@gmail.com>
2015-05-18 22:27:09 +0000

    added devise views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @items = @user.items.visible_to(current_user)
+
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash.now[:notice] = "User information updated"
+      redirect_to edit_user_registration_path
+    else
+      flash.now[:error] = "Invalid user information"
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+  
+  
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_action :authenticate_user!
+  
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  
   
   
   
